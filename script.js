49d0f30797250a5bb02ba0aaa5a481ae1c6df4a9
sandajama <sandajama29@gmail.com>
2023-06-25 19:54:38 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Good Luck 💪🏾
+function brokenKeys(correctWord, wrongWord){
+    let brokenKey = [];
+     for (let i = 0; i < correctWord.length; i++) {
+          if (correctWord[i] !== wrongWord[i]) { 
+            if (!brokenKey.includes(correctWord[i])) {
+              brokenKey.push(correctWord[i]);
+            }
+          }
+        }
+        return brokenKey;
+      } 
+    
+    
+console.log(brokenKeys("fikrcamp bootcamp", "fikrcomp boorcamp")) //➞ ["a","t"]
+console.log(brokenKeys("good night", "gppd night"))// ➞ ["o"]
